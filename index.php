e9a3cef02725f0b79b78ee1c38c9f600186fafcb
Tirea Aean <tirea@learnnavi.org>
2019-08-03 05:29:27 -0400

    jQuery 2.2.0

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
         are the properties of their respective owners.
       </p>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
