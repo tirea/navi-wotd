36b2a305e76c6b0b8ca64db71d792a4623fb615f
Tirea Aean <tirea@learnnavi.org>
2019-08-03 05:08:13 -0400

    jQuery for the sidenav

--- index.php
+++ index.php
@@ -76,10 +76,10 @@
     </div>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems, options);
+      $(document).ready(function(){
+        $('.sidenav').sidenav();
       });
     </script>
   </body>
