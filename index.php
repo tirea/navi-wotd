a6ac7b1e2b24424071809bdeaf2dd2edcb46e50d
Tirea Aean <tirea@learnnavi.org>
2019-08-03 05:27:49 -0400

    fix sidenav wth

--- index.php
+++ index.php
@@ -76,9 +76,9 @@
         are the properties of their respective owners.
       </p>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
       $(document).ready(function(){
         $('.button-collapse').sideNav();
